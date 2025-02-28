41a939ef0e97041133530c3f98cf7cb5564a4fc0
ecorvee <ecorvee@luminess.com>
2025-02-28 14:33:56 +0100

    [master] add videos

--- server.py
+++ server.py
@@ -10,6 +10,10 @@ def serve_index():
     return send_from_directory(".", "index.html")
     # return render_template("index.html")
 
+@app.route("/static/videos/<path:filename>")
+def serve_video(filename):
+    return send_from_directory("static/videos", filename, mimetype="video/mp4")  # Ensure correct MIME type
+
 # @app.route("/<path:filename>")
 # def serve_static(filename):
 #     return send_from_directory(".", filename, mimetype=get_mime_type(filename))
